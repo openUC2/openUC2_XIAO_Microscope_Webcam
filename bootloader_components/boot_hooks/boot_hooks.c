d5ae02e42a77e30cf4fca4f075ec12e7b0fdabd2
beniroquai <bene.d@gmx.de>
2025-03-21 20:43:20 +0100

    reverting to basic build config

--- bootloader_components/boot_hooks/boot_hooks.c
+++ bootloader_components/boot_hooks/boot_hooks.c
@@ -6,8 +6,6 @@
 #include "esp_log.h"
 #include "soc/rtc_cntl_struct.h"
 #include "soc/usb_serial_jtag_reg.h"
-#include "rom/ets_sys.h"
-
 
 /* Function used to tell the linker to include this file
  * with all its symbols.
@@ -22,5 +20,4 @@ void bootloader_before_init(void)
     SET_PERI_REG_MASK(USB_SERIAL_JTAG_CONF0_REG, USB_SERIAL_JTAG_PAD_PULL_OVERRIDE);
     CLEAR_PERI_REG_MASK(USB_SERIAL_JTAG_CONF0_REG, USB_SERIAL_JTAG_DP_PULLUP);
     CLEAR_PERI_REG_MASK(USB_SERIAL_JTAG_CONF0_REG, USB_SERIAL_JTAG_USB_PAD_ENABLE);
-    
 }
